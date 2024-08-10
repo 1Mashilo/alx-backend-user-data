56f470e91d5440a855bde802bc73573d2acb836f
1Mashilo <christophermashilo@gmail.com>
2024-08-10 21:41:17 +0200

    ✅ Complete task 0

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -6,33 +6,33 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-
+from api.v1.auth.auth import Auth  # Import your specific authentication class
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = Auth()  # Initialize the authentication class
+
+@app.before_request
+def before_request():
+    """ Before request method to handle authentication """
+    request.current_user = auth.current_user(request)  # Assign the current user
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
-
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler.
-    """
+    """Forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
-
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
