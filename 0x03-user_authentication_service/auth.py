7f48d72ce04f6ec21021e2e41fb08dde5d459ea6
1mashilo <christophermashilo@gmail.com>
2024-08-15 22:53:03 +0000

    ✅ Complete task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,13 +5,14 @@ This module provides a function to hash passwords using bcrypt.
 
 import bcrypt
 
+
 def _hash_password(password: str) -> bytes:
     """
     Hashes a password using bcrypt and returns the hashed password.
-    
+
     Args:
         password (str): The password to hash.
-        
+
     Returns:
         bytes: The salted hash of the password.
     """
