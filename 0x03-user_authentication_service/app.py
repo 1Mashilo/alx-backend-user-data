f2392bd7acff933d171f663d7a9f817a7b2ba90a
1Mashilo <christophermashilo@gmail.com>
2024-08-16 20:30:54 +0200

    🎨 Made code PEP 8 compliant

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -10,6 +10,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def index():
     """
@@ -19,6 +20,7 @@ def index():
         A JSON response with a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
