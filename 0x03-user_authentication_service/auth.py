804d2e75248aaca331ff413597b162b6b7457408
1Mashilo <christophermashilo@gmail.com>
2024-08-16 20:09:03 +0200

    🔨 Refactored _hash_password to remove unnecessary self parameter

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,5 +45,5 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             return self._db.add_user(email, hashed_password)
